765e0b9e2fd6f058d260b8225689f784b4831973
Vassilis Virvilis <vasvir2@gmail.com>
2026-01-14 00:27:09 +0200

    move preety print debug functions from utils.rs to simd/mod.rs

--- src/utils.rs
+++ src/utils.rs
@@ -33,10 +33,6 @@ use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::prelude::*;
 
 use crate::prelude::*;
-use crate::simd::__m128i;
-use crate::simd::__m256i;
-use crate::simd::_mm_storeu_si128;
-use crate::simd::_mm256_storeu_si256;
 
 pub fn configure_tracing<P: AsRef<Path>>(
     stderr_log_level: Level,
@@ -132,78 +128,6 @@ impl Default for SerialMap {
     fn default() -> Self {
         Self::new()
     }
-}
-
-#[allow(dead_code)]
-pub fn print_vec_char_128_dec(x: __m128i) {
-    let mut v = [0u8; 16];
-    // SAFETY: dst is 16 * 8 = bytes
-    unsafe {
-        _mm_storeu_si128(v.as_mut_ptr().cast::<__m128i>(), x);
-    }
-    println!(
-        "{:0>2} {:0>2} {:0>2} {:0>2} | {:0>2} {:0>2} {:0>2} {:0>2} | {:0>2} {:0>2} {:0>2} {:0>2} | {:0>2} {:0>2} {:0>2} {:0>2}",
-        v[15],
-        v[14],
-        v[13],
-        v[12],
-        v[11],
-        v[10],
-        v[9],
-        v[8],
-        v[7],
-        v[6],
-        v[5],
-        v[4],
-        v[3],
-        v[2],
-        v[1],
-        v[0]
-    );
-}
-
-#[allow(dead_code)]
-pub fn print_vec_char_256_hex(x: __m256i) {
-    let mut v = [0u8; 32];
-    // SAFETY: dst is 32 * 8 = bytes
-    unsafe {
-        _mm256_storeu_si256(v.as_mut_ptr().cast::<__m256i>(), x);
-    }
-    println!(
-        "{:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x} || {:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x} | {:0>2x} {:0>2x} {:0>2x} {:0>2x}",
-        v[31],
-        v[30],
-        v[29],
-        v[28],
-        v[27],
-        v[26],
-        v[25],
-        v[24],
-        v[23],
-        v[22],
-        v[21],
-        v[20],
-        v[19],
-        v[18],
-        v[17],
-        v[16],
-        v[15],
-        v[14],
-        v[13],
-        v[12],
-        v[11],
-        v[10],
-        v[9],
-        v[8],
-        v[7],
-        v[6],
-        v[5],
-        v[4],
-        v[3],
-        v[2],
-        v[1],
-        v[0]
-    );
 }
 
 /// Computes the number of chunks that will result from splitting a collection

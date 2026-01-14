5b593fb5a7a266e76366b1a36b33cabc68fce991
Vassilis Virvilis <vasvir2@gmail.com>
2026-01-14 12:26:10 +0200

    move SSE2 macros to sse_base.rs

--- src/simd/sse2_base.rs
+++ src/simd/sse2_base.rs
@@ -46,6 +46,68 @@ pub struct __m256i {
     pub high: __m128i,
 }
 
+/**
+ * We use these macros here to have the same body (implementation)
+ * for SSE2 and SSE3 _mm256_* emulation while calling target specific
+ * intrinsics without safety warnings.
+ */
+macro_rules! _mm256_shuffle_epi8 {
+    ($a:expr, $b:expr) => {{
+        // We must shuffle 'low' and 'high' independently to match AVX2 behavior.
+        __m256i {
+            low: _mm_shuffle_epi8($a.low, $b.low),
+            high: _mm_shuffle_epi8($a.high, $b.high),
+        }
+    }};
+}
+
+/**
+ * We use these macros here to have the same body (implementation)
+ * for SSE2 and SSE4.1 _mm256_* emulation while calling target specific
+ * intrinsics without safety warnings.
+ */
+macro_rules! _mm256_extract_epi8 {
+    ($a:expr, $INDEX:expr) => {{
+        match $INDEX {
+            // Lower Lane (0-15)
+            0 => _mm_extract_epi8::<0>($a.low),
+            1 => _mm_extract_epi8::<1>($a.low),
+            2 => _mm_extract_epi8::<2>($a.low),
+            3 => _mm_extract_epi8::<3>($a.low),
+            4 => _mm_extract_epi8::<4>($a.low),
+            5 => _mm_extract_epi8::<5>($a.low),
+            6 => _mm_extract_epi8::<6>($a.low),
+            7 => _mm_extract_epi8::<7>($a.low),
+            8 => _mm_extract_epi8::<8>($a.low),
+            9 => _mm_extract_epi8::<9>($a.low),
+            10 => _mm_extract_epi8::<10>($a.low),
+            11 => _mm_extract_epi8::<11>($a.low),
+            12 => _mm_extract_epi8::<12>($a.low),
+            13 => _mm_extract_epi8::<13>($a.low),
+            14 => _mm_extract_epi8::<14>($a.low),
+            15 => _mm_extract_epi8::<15>($a.low),
+            // Upper Lane (16-31)
+            16 => _mm_extract_epi8::<0>($a.high),
+            17 => _mm_extract_epi8::<1>($a.high),
+            18 => _mm_extract_epi8::<2>($a.high),
+            19 => _mm_extract_epi8::<3>($a.high),
+            20 => _mm_extract_epi8::<4>($a.high),
+            21 => _mm_extract_epi8::<5>($a.high),
+            22 => _mm_extract_epi8::<6>($a.high),
+            23 => _mm_extract_epi8::<7>($a.high),
+            24 => _mm_extract_epi8::<8>($a.high),
+            25 => _mm_extract_epi8::<9>($a.high),
+            26 => _mm_extract_epi8::<10>($a.high),
+            27 => _mm_extract_epi8::<11>($a.high),
+            28 => _mm_extract_epi8::<12>($a.high),
+            29 => _mm_extract_epi8::<13>($a.high),
+            30 => _mm_extract_epi8::<14>($a.high),
+            31 => _mm_extract_epi8::<15>($a.high),
+            _ => panic!("Index out of bounds for 256-bit extract"),
+        }
+    }};
+}
+
 #[target_feature(enable = "sse2")]
 #[inline]
 pub fn _mm256_set_m128i(high: __m128i, low: __m128i) -> __m256i {
